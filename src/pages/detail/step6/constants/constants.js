48d3103fbbce1670252926f2eaf45482492024dc
Joohyung Park <dochipjh@gmail.com>
2025-10-29 13:20:43 +0900

    Feat: 블록 좌표 설정 constants 파일 추가

---
+++ src/pages/detail/step6/constants/constants.js
@@ -0,0 +1,16 @@
+import empty_block1 from "../assets/empty_block1.png";
+import empty_block2 from "../assets/empty_block2.png";
+import empty_block3 from "../assets/empty_block3.png";
+import empty_block4 from "../assets/empty_block4.png";
+
+import full_block1 from "../assets/full_block1.png";
+import full_block2 from "../assets/full_block2.png";
+import full_block3 from "../assets/full_block3.png";
+import full_block4 from "../assets/full_block4.png";
+
+export const TARGETS = [
+  { id: "t1", emptySrc: empty_block1, fullSrc: full_block1, x: 878, y: 90 },
+  { id: "t2", emptySrc: empty_block2, fullSrc: full_block2, x: 262, y: 310 },
+  { id: "t3", emptySrc: empty_block3, fullSrc: full_block3, x: 315, y: 435 },
+  { id: "t4", emptySrc: empty_block4, fullSrc: full_block4, x: 1135, y: 455 },
+];
