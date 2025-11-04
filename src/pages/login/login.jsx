b0e4500504116f215e07182361a7b5d7c0d9b676
chaem03 <hcm0309@naver.com>
2025-11-04 18:16:46 +0900

    Fix: 소셜로그인 vercel.json 등록

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -5,9 +5,11 @@ import FabQuest from "@assets/images/FabQuest.png";
 import KakaoImg from "@assets/images/KakaoTalk.png";
 import { LoginNickname } from "@components/login/LoginNickname";
 import { LoginBtn } from "@components/btn/LoginBtn";
+import { Link } from "react-router-dom";
+import { instance } from "@apis/instance";
 export const LoginPage = () => {
   const [Login, setLogin] = useState(false);
-  //fix
+
   return (
     <>
       <S.BackGround>
@@ -20,13 +22,15 @@ export const LoginPage = () => {
               <S.LogoImg src={FabQuest} />
               <S.LoginText>반도체 8대 공정 체험하기</S.LoginText>
             </S.TextWrapper>
-            <S.BtnContainer>
-              <LoginBtn
-                onClick={() => setLogin(true)}
-                imgSrc={KakaoImg}
-                text={" 카카오로 시작하기"}
-              />
-            </S.BtnContainer>
+            <Link to="https://fabquest.oss-project-six.shop/oauth2/authorization/kakao">
+              <S.BtnContainer>
+                <LoginBtn
+                  onClick={() => setLogin(true)}
+                  imgSrc={KakaoImg}
+                  text={" 카카오로 시작하기"}
+                />
+              </S.BtnContainer>
+            </Link>
           </S.LogoWrapper>
         )}
       </S.BackGround>
